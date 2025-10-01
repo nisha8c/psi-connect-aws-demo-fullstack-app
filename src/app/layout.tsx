ab96a1156d707fe641fe627f430f78e965b2339a
Nisha Chavan <nisha.chavan@lekab.com>
2025-10-01 11:22:18 +0200

    no change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <AmplifyInit /> {/* configures Amplify once, client-side */}
+      <AmplifyInit />  {/* configures Amplify once, client-side */}
         {children}
       </body>
     </html>
